2915c7474f725546eece145fa2dafd7e266c39ea
Don Williams <don.e.williams@gmail.com>
2025-12-26 19:49:15 -0500

    Updated nixvim.nix to current version
    It's not nearly identical to bugsvim.nix
    
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   config/editors/nixvim.nix

--- config/editors/nixvim.nix
+++ config/editors/nixvim.nix
@@ -79,7 +79,65 @@
 
       # Notifications and UI polish
       notify.enable = true;
-      noice.enable = true;
+      noice = {
+        enable = true;
+        settings = {
+          lsp = {
+            override = {
+              "vim.lsp.util.convert_input_to_markdown_lines" = true;
+              "vim.lsp.util.stylize_markdown" = true;
+              "cmp.entry.get_documentation" = true;
+            };
+          };
+          presets = {
+            bottom_search = true;
+            command_palette = true;
+            long_message_to_split = true;
+            inc_rename = false;
+            lsp_doc_border = false;
+          };
+        };
+      };
+
+      # snacks.nvim - QoL improvements and pickers
+      snacks = {
+        enable = true;
+        settings = {
+          bigfile = {enabled = true;};
+          dashboard = {
+            enabled = true;
+            preset = {
+              header = ''
+                ███╗   ██╗██╗██╗  ██╗██╗   ██╗██╗███╗   ███╗
+                ████╗  ██║██║╚██╗██╔╝██║   ██║██║████╗ ████║
+                ██╔██╗ ██║██║ ╚███╔╝ ██║   ██║██║██╔████╔██║
+                ██║╚██╗██║██║ ██╔██╗ ╚██╗ ██╔╝██║██║╚██╔╝██║
+                ██║ ╚████║██║██╔╝ ██╗ ╚████╔╝ ██║██║ ╚═╝ ██║
+                ╚═╝  ╚═══╝╚═╝╚═╝  ╚═╝  ╚═══╝  ╚═╝╚═╝     ╚═╝
+              '';
+            };
+          };
+          explorer = {enabled = true;};
+          indent = {enabled = true;};
+          image = {enabled = true;};
+          input = {enabled = true;};
+          notifier = {
+            enabled = true;
+            timeout = 3000;
+          };
+          picker = {enabled = true;};
+          quickfile = {enabled = true;};
+          scope = {enabled = true;};
+          scroll = {enabled = true;};
+          statuscolumn = {enabled = true;};
+          words = {enabled = true;};
+          styles = {
+            notification = {
+              wo = {wrap = true;};
+            };
+          };
+        };
+      };
 
       # Startup dashboard
       alpha = {
@@ -96,7 +154,35 @@
       leap.enable = true;
       vim-surround.enable = true;
       comment.enable = true;
-      which-key.enable = true;
+
+      # mini.nvim - Collection of independent Lua modules for editing
+      mini = {
+        enable = true;
+        mockDevIcons = true;
+        modules = {
+          ai = {};
+          comment = {};
+          move = {};
+          surround = {};
+          cursorword = {};
+          pairs = {};
+          trailspace = {};
+          icons = {};
+        };
+      };
+
+      # TODO/HACK/BUG comment highlighting and navigation
+      todo-comments = {
+        enable = true;
+      };
+
+      # Keybinding hints and display
+      which-key = {
+        enable = true;
+        settings = {
+          preset = "helix";
+        };
+      };
 
       # Autopairs for (), {}, [], '', "", etc.
       nvim-autopairs = {
@@ -125,14 +211,38 @@
       markdown-preview.enable = true;
 
       # Completion and snippets
-      cmp = {
+      blink-cmp = {
         enable = true;
+        settings = {
+          keymap = {
+            preset = "default";
+            "<CR>" = ["accept" "fallback"];
+            "<Tab>" = ["select_next" "fallback"];
+            "<S-Tab>" = ["select_prev" "fallback"];
+          };
+          appearance = {
+            nerd_font_variant = "mono";
+          };
+          completion = {
+            documentation = {
+              auto_show = true;
+              auto_show_delay_ms = 500;
+            };
+          };
+          sources = {
+            default = ["lsp" "path" "snippets" "buffer"];
+          };
+          snippets = {
+            preset = "luasnip";
+          };
+          fuzzy = {
+            implementation = "prefer_rust_with_warning";
+          };
+          signature = {
+            enabled = true;
+          };
+        };
       };
-      # Completion sources for LSP, buffer, path, and snippets
-      cmp-nvim-lsp.enable = true;
-      cmp-buffer.enable = true;
-      cmp-path.enable = true;
-      cmp_luasnip.enable = true;
 
       luasnip.enable = true;
       friendly-snippets.enable = true;
@@ -155,6 +265,18 @@
           marksman.enable = true;
           hyprls.enable = true;
           # hyprls is optional; keep tools available via extraPackages
+          tailwindcss = {
+            enable = true;
+            filetypes = [
+              "javascript"
+              "javascriptreact"
+              "typescript"
+              "typescriptreact"
+              "vue"
+              "svelte"
+            ];
+          };
+          bashls.enable = true;
         };
         keymaps = {
           diagnostic = {
@@ -171,6 +293,7 @@
         settings = {
           formatters_by_ft = {
             nix = ["alejandra"];
+            #nix = [ "nixpkgs_fmt" ];
             lua = ["stylua"];
             javascript = ["prettierd"];
             typescript = ["prettierd"];
@@ -274,6 +397,116 @@
         options.desc = "Toggle diagnostics list";
       }
 
+      # Snacks: Terminal and scratch buffers
+      {
+        key = "<c-/>";
+        mode = ["n"];
+        action = "<cmd>lua Snacks.terminal()<CR>";
+        options.desc = "Toggle terminal";
+      }
+      {
+        key = "<leader>.";
+        mode = ["n"];
+        action = "<cmd>lua Snacks.scratch()<CR>";
+        options.desc = "Toggle scratch buffer";
+      }
+      {
+        key = "<leader>S";
+        mode = ["n"];
+        action = "<cmd>lua Snacks.scratch.select()<CR>";
+        options.desc = "Select scratch buffer";
+      }
+
+      # Snacks: Zen and zoom modes
+      {
+        key = "<leader>z";
+        mode = ["n"];
+        action = "<cmd>lua Snacks.zen()<CR>";
+        options.desc = "Toggle zen mode";
+      }
+      {
+        key = "<leader>Z";
+        mode = ["n"];
+        action = "<cmd>lua Snacks.zen.zoom()<CR>";
+        options.desc = "Toggle zoom";
+      }
+
+      # Snacks: Git
+      {
+        key = "<leader>gg";
+        mode = ["n"];
+        action = "<cmd>lua Snacks.lazygit()<CR>";
+        options.desc = "Lazygit";
+      }
+      {
+        key = "<leader>gB";
+        mode = ["n" "v"];
+        action = "<cmd>lua Snacks.gitbrowse()<CR>";
+        options.desc = "Git browse";
+      }
+
+      # Snacks: Buffer and file operations
+      {
+        key = "<leader>bd";
+        mode = ["n"];
+        action = "<cmd>lua Snacks.bufdelete()<CR>";
+        options.desc = "Delete buffer";
+      }
+      {
+        key = "<leader>cR";
+        mode = ["n"];
+        action = "<cmd>lua Snacks.rename.rename_file()<CR>";
+        options.desc = "Rename file";
+      }
+
+      # Snacks: Notifications
+      {
+        key = "<leader>n";
+        mode = ["n"];
+        action = "<cmd>lua Snacks.notifier.show_history()<CR>";
+        options.desc = "Notification history";
+      }
+      {
+        key = "<leader>un";
+        mode = ["n"];
+        action = "<cmd>lua Snacks.notifier.hide()<CR>";
+        options.desc = "Dismiss all notifications";
+      }
+
+      # TODO comments navigation
+      {
+        key = "]t";
+        mode = ["n"];
+        action = "<cmd>lua require('todo-comments').jump_next()<CR>";
+        options.desc = "Next todo comment";
+      }
+      {
+        key = "[t";
+        mode = ["n"];
+        action = "<cmd>lua require('todo-comments').jump_prev()<CR>";
+        options.desc = "Previous todo comment";
+      }
+      {
+        key = "<leader>st";
+        mode = ["n"];
+        action = "<cmd>TodoTelescope<CR>";
+        options.desc = "Search todo comments";
+      }
+      {
+        key = "<leader>sT";
+        mode = ["n"];
+        action = "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<CR>";
+        options.desc = "Search TODO/FIX/FIXME comments";
+      }
+
+      # Which-key buffer keymaps
+      {
+        key = "<leader>?";
+        mode = ["n"];
+        action = "<cmd>lua require('which-key').show({ global = false })<CR>";
+        options.desc = "Buffer local keymaps (which-key)";
+      }
+
       # Disable accidental F1 across modes
       {
         key = "<F1>";
@@ -301,47 +534,103 @@
 
     # Runtime tools and language servers
     extraPackages = with pkgs; [
-      chafa # Image privew
-      clang-tools
       bat
+      chafa
+      clang-tools
       fd
       figlet
       hyprls
       lazygit
-      lua-language-server
-      marksman
       nil
+      nixpkgs-fmt
       nodePackages.typescript-language-server
       nodePackages.typescript
-      nixpkgs-fmt
-      prettierd
       pyright
+      lua-language-server
+      marksman
+      prettierd
       ripgrep
       stylua
       shfmt
       toilet
       wl-clipboard
-      viu # Better image prview need make work
+      viu
       vscode-langservers-extracted
       zls
+      tailwindcss-language-server
+      nodePackages.bash-language-server
     ];
 
     extraPlugins = with pkgs.vimPlugins; [
       telescope-media-files-nvim
+      nui-nvim  # Required by snacks and noice
     ];
 
     # Diagnostic UI and notify background tweaks
     extraConfigLua = ''
-
       require('telescope').load_extension('media_files')
 
-      -- Inline diagnostics (virtual text) similar to NVF virtual_lines
+      -- Diagnostic configuration with enhanced icons and styling (from bugsvim)
+      local diagnostic_signs = {
+        Error = " ",
+        Warn = " ",
+        Hint = "",
+        Info = "",
+      }
+
       vim.diagnostic.config({
-        virtual_text = { prefix = "●", spacing = 2 },
-        update_in_insert = true,
-        severity_sort = true,
+        -- Show signs in the gutter with custom icons
+        signs = {
+          text = {
+            [vim.diagnostic.severity.ERROR] = diagnostic_signs.Error,
+            [vim.diagnostic.severity.WARN] = diagnostic_signs.Warn,
+            [vim.diagnostic.severity.INFO] = diagnostic_signs.Info,
+            [vim.diagnostic.severity.HINT] = diagnostic_signs.Hint,
+          },
+          numhl = {
+            [vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
+            [vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
+            [vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
+            [vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
+          },
+          linehl = false,
+        },
+        -- Show virtual text next to error (inline diagnostics)
+        virtual_text = {
+          prefix = "●",
+          spacing = 2,
+          severity = {
+            min = vim.diagnostic.severity.HINT,
+          },
+        },
+        -- Show diagnostics on separate lines for better visibility
+        virtual_lines = {
+          only_current_line = false,
+          highlight_whole_line = false,
+        },
+        -- Underline errors
         underline = true,
-        signs = true,
+        -- Show diagnostic in floating window on hover with source
+        float = {
+          source = "always",
+          border = "rounded",
+          pad_bottom = 1,
+        },
+        severity_sort = true,
+        update_in_insert = false,
+      })
+
+      -- Highlight the yanked text for 200ms
+      local highlight_yank_group = vim.api.nvim_create_augroup('HighlightYank', {})
+      vim.api.nvim_create_autocmd('TextYankPost', {
+        group = highlight_yank_group,
+        pattern = '*',
+        callback = function()
+          vim.hl.on_yank {
+            higroup = 'IncSearch',
+            timeout = 200,
+          }
+        end,
       })
 
       -- Basic LSP keymaps when LSP attaches
@@ -372,48 +661,21 @@
       -- Notify background using Stylix palette
       local ok, notify = pcall(require, 'notify')
       if ok then
-        -- Use a Catppuccin Mocha background color directly to avoid Stylix dependency
-        notify.setup({ background_colour = "#1e1e2e" })
+        notify.setup({ background_colour = "#${config.lib.stylix.colors.base01}" })
         vim.notify = notify
       end
 
-      -- nvim-cmp setup: sources + mappings + snippet expansion
+      -- blink-cmp setup: configured via nixvim opts above, with luasnip integration
       do
-        local ok_cmp, cmp = pcall(require, "cmp")
-        if ok_cmp then
-          local ok_snip, luasnip = pcall(require, "luasnip")
-          if ok_snip then
-            -- Load friendly-snippets lazily if available
-            pcall(function()
-              require("luasnip.loaders.from_vscode").lazy_load()
-            end)
-          end
-          cmp.setup({
-            snippet = {
-              expand = function(args)
-                if ok_snip then luasnip.lsp_expand(args.body) end
-              end,
-            },
-            mapping = cmp.mapping.preset.insert({
-              ["<C-Space>"] = cmp.mapping.complete(),
-              ["<CR>"] = cmp.mapping.confirm({ select = true }),
-              ["<Tab>"] = cmp.mapping.select_next_item(),
-              ["<S-Tab>"] = cmp.mapping.select_prev_item(),
-            }),
-            sources = cmp.config.sources({
-              { name = "nvim_lsp" },
-              { name = "luasnip" },
-            }, {
-              { name = "path" },
-              { name = "buffer" },
-            }),
-          })
-          -- nvim-cmp integration with nvim-autopairs (optional)
-          local ok_ap, cmp_autopairs = pcall(require, "nvim-autopairs.completion.cmp")
-          if ok_ap then
-            cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-          end
+        local ok_snip, luasnip = pcall(require, "luasnip")
+        if ok_snip then
+          -- Load friendly-snippets lazily if available
+          pcall(function()
+            require("luasnip.loaders.from_vscode").lazy_load()
+          end)
         end
+        -- blink-cmp configuration is handled through the opts table above
+        -- No additional setup needed here
       end
 
       -- Startup dashboard (alpha-nvim)
@@ -437,28 +699,29 @@
             return #lines > 0 and lines or nil
           end
 
-          header_lines = gen_banner('toilet -f ansi-shadow NIXOS 2>/dev/null')
-            or gen_banner('figlet -f "ANSI Shadow" NIXOS 2>/dev/null')
-            or gen_banner('figlet NIXOS 2>/dev/null')
+          header_lines = gen_banner('toilet -f ansi-shadow NIXVIM 2>/dev/null')
+            or gen_banner('figlet -f "ANSI Shadow" NIXVIM 2>/dev/null')
+            or gen_banner('figlet NIXVIM 2>/dev/null')
 
           if not header_lines then
             header_lines = {
-              " _   _ ___  __  __  ___   ___   ____  ",
-              "| \\ | |_ _| \\ \\/ / / _ \\ / _ \\\ |  _ \\ ",
-              "|  \\| || |   \\  / | | | | | | || | | |",
-              "| |\\  || |   /  \\ | |_| | |_| || |_| |",
-              "|_| \\_|___| /_/\\_\\ \\___/ \\___/ |____/ ",
+              "███╗   ██╗██╗██╗  ██╗██╗   ██╗██╗███╗   ███╗",
+              "████╗  ██║██║╚██╗██╔╝██║   ██║██║████╗ ████║",
+              "██╔██╗ ██║██║ ╚███╔╝ ██║   ██║██║██╔████╔██║",
+              "██║╚██╗██║██║ ██╔██╗ ╚██╗ ██╔╝██║██║╚██╔╝██║",
+              "██║ ╚████║██║██╔╝ ██╗ ╚████╔╝ ██║██║ ╚═╝ ██║",
+              "╚═╝  ╚═══╝╚═╝╚═╝  ╚═╝  ╚═══╝  ╚═╝╚═╝     ╚═╝",
             }
           end
           dashboard.section.header.val = header_lines
 
           dashboard.section.buttons.val = {
-            dashboard.button("f", "  Find file", ":Telescope find_files<CR>"),
-            dashboard.button("r", "  Recent files", ":Telescope oldfiles<CR>"),
+            dashboard.button("f", "  Find file", ":Telescope find_files<CR>"),
+            dashboard.button("r", "  Recent files", ":Telescope oldfiles<CR>"),
             dashboard.button("g", "󰺮  Live grep", ":Telescope live_grep<CR>"),
-            dashboard.button("n", "  New file", ":enew<CR>"),
-            dashboard.button("e", "  File browser", ":Neotree toggle<CR>"),
-            dashboard.button("q", "  Quit", ":qa<CR>"),
+            dashboard.button("n", "  New file", ":enew<CR>"),
+            dashboard.button("e", "  File browser", ":Neotree toggle<CR>"),
+            dashboard.button("q", "  Quit", ":qa<CR>"),
           }
 
           local v = vim.version()
